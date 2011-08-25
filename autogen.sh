729d3b4ddbbca86ee94d2b332f929d3ed4930f29
Prakash Surya <surya1@llnl.gov>
2011-08-25 12:32:47 -0700

    Remove remaining disabled configure.ac features
    The remaining features of the configure.ac script that were disabled
    have been stripped out by this commit. The remaining portions of the
    configure.ac script are what has been deemed necessary to build the
    package. As a result, the dependency on the build/autoconf/*.m4 macros
    was removed from the autogen.sh script.

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-#
-# TODO: Old m4 macros are in the build/autoconf directory. Thus after
-# cleanup is finished this should be removed.
-#
-aclocal -I m4 -I build/autoconf
+aclocal -I m4
 autoheader
 
 # Only need no-portability if version > 1.10
